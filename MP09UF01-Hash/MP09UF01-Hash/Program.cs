6b79cfaef1b2c2b2982a32a19091dfef2e938f84
RobeEdu2400 <robe_edu@hotmail.com>
2017-09-29 15:50:12 +0200

    Menu

--- MP09UF01-Hash/MP09UF01-Hash/Program.cs
+++ MP09UF01-Hash/MP09UF01-Hash/Program.cs
@@ -10,6 +10,26 @@ namespace MP09UF01_Hash
     {
         static void Main(string[] args)
         {
+            int menu = 1;
+
+            while (menu != 0) {
+                Console.WriteLine("Introduce 1 para crear el hash.");
+                Console.WriteLine("Introduce 2 para comprobar la integridad");
+                Console.WriteLine("Introduce 0 para salir.");
+                menu = Convert.ToInt32(Console.ReadLine());
+
+                if (menu == 1)
+                {
+
+                }
+                else
+                {
+                    if (menu == 2)
+                    {
+
+                    }
+                }
+            }
         }
     }
 }
