180fafdae37aacc4a3c80ffc54b863f42a3f6d18
RobeEdu2400 <robe_edu@hotmail.com>
2017-09-29 16:29:58 +0200

    Finalizado

--- MP09UF01-Hash/MP09UF01-Hash/Program.cs
+++ MP09UF01-Hash/MP09UF01-Hash/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,7 +13,7 @@ namespace MP09UF01_Hash
         static void Main(string[] args)
         {
             int menu = 1;
-
+            String texto;
             while (menu != 0) {
                 Console.WriteLine("Introduce 1 para crear el hash.");
                 Console.WriteLine("Introduce 2 para comprobar la integridad");
@@ -20,16 +22,76 @@ namespace MP09UF01_Hash
 
                 if (menu == 1)
                 {
+                    String nombreArchivo=null;
+                    Console.WriteLine("Entra el nombre del archivo: ");
+                    
+                    while (nombreArchivo == null)
+                        nombreArchivo = Console.ReadLine();
+                    texto = File.ReadAllText(@nombreArchivo);
 
+                    String hash;
+                    hash=calcularHash(texto);
+
+                    Console.WriteLine("Introduce el nombre del archivo:");
+                    String nombre = null;
+                    while (nombre == null)
+                        nombre = Console.ReadLine();
+
+                    File.WriteAllText(nombre, hash);
                 }
                 else
                 {
                     if (menu == 2)
                     {
+                        String nombreArchivo = null;
+                        Console.WriteLine("Entra el nombre del archivo: ");
 
+                        while (nombreArchivo == null)
+                            nombreArchivo = Console.ReadLine();
+                        texto = File.ReadAllText(@nombreArchivo);
+
+                        String hash;
+                        String nombreHash=null;
+                        String hashGuardado;
+                        hash = calcularHash(texto);
+
+                        Console.Write("Entra el nombre del hash: ");
+                        while (nombreHash == null)
+                            nombreHash = Console.ReadLine();
+
+                        hashGuardado = File.ReadAllText(nombreHash);
+
+                        if (hash == hashGuardado)
+                        {
+                            Console.WriteLine("Es Correcto");
+                        }
+                        else
+                        {
+                            Console.WriteLine("No es Correcto");
+                        }
                     }
                 }
             }
+        }
+
+        private static string calcularHash(string texto)
+        {
+            // Convertim l'string a un array de bytes
+            byte[] bytesIn = UTF8Encoding.UTF8.GetBytes(texto);
+            // Instanciar classe per fer hash
+            SHA512Managed SHA512 = new SHA512Managed();
+            // Calcular hash
+            byte[] hashResult = SHA512.ComputeHash(bytesIn);
+
+            // Si volem mostrar el hash per pantalla o guardar-lo en un arxiu de text
+            // cal convertir-lo a un string
+
+            String hash = BitConverter.ToString(hashResult, 0);  
+
+
+            // Eliminem la classe instanciada
+            SHA512.Dispose();
+            return hash;
         }
     }
 }
